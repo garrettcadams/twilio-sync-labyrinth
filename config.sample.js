2e016ed41f8a301730b892ce7071f366e3be76d5
garrettcadams <garrettcadams@users.noreply.github.com>
2017-02-26 16:49:00 -0500

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,23 +1,23 @@
 module.exports = {
     // Your primary Twilio Account SID
-    accountSid: 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    accountSid: 'AC9ac47d360961650c8350b4facffc0bc2',
 
     // Twilio Auth Token
-    authToken: 'your_auth_token',
+    authToken: 'bf10c7ad8e9f95ea0ad53715142083c5',
 
     // Twilio Number from which to send text messages
     twilioNumber: '+15017250604',
     sendTextMessages: true,
-    domain: 'https://xxxxxxx.ngrok.io',
+    domain: 'https://bd1fef04.ngrok.io',
 
     // API Key/Secret Pair - generate a pair in the console
-    apiKey: 'SKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    apiSecret: 'your_api_secret',
+    apiKey: 'SK61d8e4ceedf5f75c3b9adf8568488b08',
+    apiSecret: 'AZmwA7sTqAiYqOZPYe7VQCDnNZgjlMjQ',
 
     // Your Sync service instance SID
-    serviceSid: 'ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    serviceSid: 'test',
     
     // Your Copilot SID
     // https://www.twilio.com/user/account/messaging/services
-    messagingServiceSid: 'MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    messagingServiceSid: 'MG0913c7d43d428d5a8757a72266d8ebfd'
 };
